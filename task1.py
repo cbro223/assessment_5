49ad9962ca69a52aafdb527a732b672e8fa3872a
cbro223 <cbro223@aucklanduni.ac.nz>
2024-07-12 14:28:24 +1200

    Another change

--- task1.py
+++ task1.py
@@ -5,5 +5,4 @@ difference_squares(1 + 2 ** -29, 1 + 2 ** -30)
 # Find the relative error of this subtraction
 relative_error_subtraction(np.float64(1 + (1 * (10 ** -15))), np.float64(1 + (2 * (10 ** -15))), -1 * (10 ** -15))
 
-#some change
 pass
